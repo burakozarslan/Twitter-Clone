0929a8b692799d30ead27f7b24f9933fbe170367
Burak Ozarslan <burakozarslan@icloud.com>
2022-06-13 06:38:24 +0300

    db 💾 add extra fields to user model

---
+++ db/migrations/20220613030427_add_username_field_to_user_model/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT,
+    "avatar" TEXT,
+    "bannerImage" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER'
+);
+INSERT INTO "new_User" ("avatar", "bannerImage", "createdAt", "email", "hashedPassword", "id", "name", "role", "updatedAt") SELECT "avatar", "bannerImage", "createdAt", "email", "hashedPassword", "id", "name", "role", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;

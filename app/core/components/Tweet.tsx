97e939a50302b527f6acfdb61b7e5abd1a5f2684
Burak Ozarslan <burakozarslan@icloud.com>
2022-06-22 15:25:31 +0300

    style 💎 fix tweet alignment

--- app/core/components/Tweet.tsx
+++ app/core/components/Tweet.tsx
@@ -11,6 +11,7 @@ import Fade from "@mui/material/Fade"
 import Button from "@mui/material/Button"
 import IconButton from "@mui/material/IconButton"
 import Link from "@mui/material/Link"
+import Stack from "@mui/material/Stack"
 // Icons
 import { BsChat, BsHeart, BsThreeDots } from "react-icons/bs"
 
@@ -55,125 +56,132 @@ const Tweet = ({
     <Box
       sx={{
         display: "flex",
-        justifyContent: "space-between",
         alignItems: "start",
+        justifyContent: "space-between",
         borderBottom: "1px solid #eaeaea",
         p: 1,
       }}
     >
-      <Avatar
-        src={authorAvatar || "https://via.placeholder.com/150"}
+      <Stack direction="row">
+        <Avatar
+          src={authorAvatar || "https://via.placeholder.com/150"}
+          sx={{
+            width: 50,
+            height: 50,
+          }}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            px: 1,
+          }}
+        >
+          {/* Tweet Info */}
+          <Box
+            sx={{
+              display: "flex",
+
+              span: {
+                ml: 0.5,
+                fontSize: 15,
+              },
+            }}
+          >
+            <Box onMouseEnter={handleOpenPopper} onMouseLeave={handleClosePopper}>
+              <BlitzLink href={Routes.ProfilePage({ username: authorUsername })} passHref>
+                <Link underline="hover" color="#222">
+                  <Typography aria-describedby={id} sx={{ fontWeight: "bold" }} component="span">
+                    {authorName}
+                  </Typography>
+                </Link>
+              </BlitzLink>
+              <Popper id={id} open={open} anchorEl={anchorEl} transition>
+                {({ TransitionProps }) => (
+                  <Fade {...TransitionProps} timeout={350}>
+                    <Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>
+                      <Button
+                        variant="contained"
+                        onClick={() =>
+                          unfollowMutation({
+                            id: authorId,
+                          })
+                        }
+                      >
+                        Unfollow
+                      </Button>
+                    </Box>
+                  </Fade>
+                )}
+              </Popper>
+            </Box>
+            <Typography component="span">@{authorUsername}</Typography>
+            {/* TODO: Add timestamp */}
+            <Typography component="span">4h</Typography>
+          </Box>
+          {/* Tweet Body */}
+          <Typography component="p" fontSize={15} paddingLeft={1} lineHeight={1.2}>
+            {body}
+          </Typography>
+          {/* Tweet Actions */}
+          <Box
+            sx={{
+              display: "flex",
+              mt: 1,
+            }}
+          >
+            <Button
+              sx={{
+                fontSize: 15,
+                color: "gray",
+
+                "&:hover": {
+                  color: "primary.main",
+                },
+              }}
+            >
+              <BsChat />
+              <Typography
+                component="span"
+                sx={{
+                  fontSize: 14,
+                  ml: 1,
+                }}
+              >
+                949
+              </Typography>
+            </Button>
+            <Button
+              sx={{
+                fontSize: 15,
+                color: "gray",
+                ml: 1,
+
+                "&:hover": {
+                  color: "error.main",
+                },
+              }}
+            >
+              <BsHeart />
+              <Typography
+                component="span"
+                sx={{
+                  fontSize: 14,
+                  ml: 1,
+                }}
+              >
+                12993
+              </Typography>
+            </Button>
+          </Box>
+        </Box>
+      </Stack>
+      <IconButton
+        size="small"
         sx={{
-          width: 50,
-          height: 50,
-        }}
-      />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          px: 1,
+          justifySelf: "flex-end",
         }}
       >
-        {/* Tweet Info */}
-        <Box
-          sx={{
-            display: "flex",
-
-            span: {
-              ml: 0.5,
-              fontSize: 15,
-            },
-          }}
-        >
-          <Box onMouseEnter={handleOpenPopper} onMouseLeave={handleClosePopper}>
-            <BlitzLink href={Routes.ProfilePage({ username: authorUsername })} passHref>
-              <Link underline="hover" color="#222">
-                <Typography aria-describedby={id} sx={{ fontWeight: "bold" }} component="span">
-                  {authorName}
-                </Typography>
-              </Link>
-            </BlitzLink>
-            <Popper id={id} open={open} anchorEl={anchorEl} transition>
-              {({ TransitionProps }) => (
-                <Fade {...TransitionProps} timeout={350}>
-                  <Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>
-                    <Button
-                      variant="contained"
-                      onClick={() =>
-                        unfollowMutation({
-                          id: authorId,
-                        })
-                      }
-                    >
-                      Unfollow
-                    </Button>
-                  </Box>
-                </Fade>
-              )}
-            </Popper>
-          </Box>
-          <Typography component="span">@{authorUsername}</Typography>
-          {/* TODO: Add timestamp */}
-          <Typography component="span">4h</Typography>
-        </Box>
-        {/* Tweet Body */}
-        <Typography component="p" fontSize={15} paddingLeft={1} lineHeight={1.2}>
-          {body}
-        </Typography>
-        {/* Tweet Actions */}
-        <Box
-          sx={{
-            display: "flex",
-            mt: 1,
-          }}
-        >
-          <Button
-            sx={{
-              fontSize: 15,
-              color: "gray",
-
-              "&:hover": {
-                color: "primary.main",
-              },
-            }}
-          >
-            <BsChat />
-            <Typography
-              component="span"
-              sx={{
-                fontSize: 14,
-                ml: 1,
-              }}
-            >
-              949
-            </Typography>
-          </Button>
-          <Button
-            sx={{
-              fontSize: 15,
-              color: "gray",
-              ml: 1,
-
-              "&:hover": {
-                color: "error.main",
-              },
-            }}
-          >
-            <BsHeart />
-            <Typography
-              component="span"
-              sx={{
-                fontSize: 14,
-                ml: 1,
-              }}
-            >
-              12993
-            </Typography>
-          </Button>
-        </Box>
-      </Box>
-      <IconButton size="small">
         <BsThreeDots />
       </IconButton>
     </Box>
